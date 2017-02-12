d3b0021235521b230a78517422178ca5bc41496a
Doug Franklin <doug@dougfranklin.com>
2017-02-12 06:24:39 -0500

    Card Component - Begin

---
+++ app/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // specify "selector"
+    selector: 'app-card',
+
+    // template (html element)
+    // Inline templates? No IDE Help
+    template: '<h1></h1>'
+})
+
+export class CardComponent{ }
