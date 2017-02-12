65a037dc8540488215a1b3eaccfc00a505ccaf6c
Doug Franklin <doug@dougfranklin.com>
2017-02-12 09:18:26 -0500

    Task class created

---
+++ app/model/task.js
@@ -0,0 +1,12 @@
+"use strict";
+// Defining the data structure
+// Creating a constructor for an instance
+var Task = (function () {
+    function Task(content, completed) {
+        this.content = content;
+        this.completed = completed;
+    }
+    return Task;
+}());
+exports.Task = Task;
+//# sourceMappingURL=task.js.map
---
+++ app/model/task.ts
@@ -0,0 +1,8 @@
+// Defining the data structure
+// Creating a constructor for an instance
+export class Task {
+    constructor (
+        public content: string,
+        public completed: boolean
+    ) { }
+}
